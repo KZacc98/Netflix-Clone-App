66bbd9d3623cb4fe453e7ae64be88f5bb4631eae
kamilzachara <kamil.zachara98@gmail.com>
2022-04-06 15:11:00 +0200

    HomeView basic layout implemented

--- Netflix Clone App/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone App/Views/CollectionViewTableViewCell.swift
@@ -11,12 +11,26 @@ class CollectionViewTableViewCell: UITableViewCell {
     
     static let identifier = "CollectionViewTableViewCell"
     
-    
+    private let collectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        //cell W/H setup
+        layout.itemSize = CGSize(width: 140, height: 200)
+        //cell scroll direction
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self , forCellWithReuseIdentifier: "cell")
+        
+        return collectionView
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
     
@@ -24,5 +38,23 @@ class CollectionViewTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError()
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
 
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
 }

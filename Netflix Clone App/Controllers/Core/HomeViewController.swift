bd685f9d7f43c8e978312bfabbb59fb0c18e3c9b
kamilzachara <kamil.zachara98@gmail.com>
2022-04-07 02:09:31 +0200

    watching trailers works across the app

--- Netflix Clone App/Controllers/Core/HomeViewController.swift
+++ Netflix Clone App/Controllers/Core/HomeViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 
+
+
 enum Sections: Int {
     case TrendingMovies = 0
     case TrendingTV = 1
@@ -17,6 +19,9 @@ enum Sections: Int {
 }
 
 class HomeViewController: UIViewController {
+    
+    private var randomTrendingMovie: Title?
+    private var headerView: HeroHeaderUIView?
     
     let sectionTitles: [String] = ["Trending movies", "Trending TV", "Popular", "Upcoming", "Top rated"]
     
@@ -37,12 +42,26 @@ class HomeViewController: UIViewController {
         
         configureNavbar()
         
-        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        
+        headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
+        configureHeroHeaderView()
         
-        
-        
+    }
+    
+    private func configureHeroHeaderView(){
+        APICaller.shared.getTrendingMovies{[weak self] result in
+            switch result {
+            case .success(let titles):
+                let selectedTitle = titles.randomElement()
+                self?.randomTrendingMovie = selectedTitle
+    
+                self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureNavbar(){
